a8e3f537e3ddc78df77eb148605c4a4d437623c9
arda <ardasahin81@gmail.com>
2025-04-09 22:07:00 +0300

    initial commit

---
+++ src/test/java/com/ardas/stockorderservice/StockOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardas.stockorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
