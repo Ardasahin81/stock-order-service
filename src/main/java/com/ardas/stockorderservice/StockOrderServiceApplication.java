a2aa074987d1321f8f631558096db6e8049db74a
arda <ardasahin81@gmail.com>
2025-04-12 11:27:29 +0300

    Enable jpa auditing

--- src/main/java/com/ardas/stockorderservice/StockOrderServiceApplication.java
+++ src/main/java/com/ardas/stockorderservice/StockOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.ardas.stockorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StockOrderServiceApplication {
 
     public static void main(String[] args) {
